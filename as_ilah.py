fa55da8d56d11669214a5e2c3362da7d4dd500c6
aissa1401r-design <aissa1401r@gmail.com>
2026-08-03 22:22:02 +0200

    Update as_ilah.py

--- as_ilah.py
+++ as_ilah.py
@@ -65,7 +65,7 @@ def send_question(chat_id):
     keyboard = {"inline_keyboard": [
         [{"text": options[0],"callback_data": f"answer_{options[0]}"},{"text":options[1], "callback_data":f"answer_{options[1]}"}],
                 [{"text":options[2],"callback_data": f"answer_{options[2]}"},{"text": options[3], "callback_data": f"answer_{options[3]}"}]
-            ]
+            ]}
 
     # نحفظو الاجابة الصحيحة في جدول users
     supabase.table("users").update({"last_answer": question["correct_answer"], "last_question_id": question["id"]}).eq("chat_id", chat_id).execute()
